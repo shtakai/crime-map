0fcefb33633c70b94fb4d6671190bdcbab7669a9
shtakai <shtakai@gmail.com>
2016-08-11 02:37:34 +0900

    fixed typo

--- db_setup.py
+++ db_setup.py
@@ -11,7 +11,7 @@ try:
         sql = "CREATE DATABASE IF NOT EXISTS crimemap"
         cursor.execute(sql)
         sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (
-        id int NOT NULL AUTO_INCLREMENT,
+        id int NOT NULL AUTO_INCREMENT,
         latitude FLOAT(10,6),
         longitude FLOAT(10,6),
         date DATETIME,
