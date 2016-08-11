d595ef45faaf07164fc03ad8498d8f34d2a52c11
shtakai <shtakai@gmail.com>
2016-08-11 17:31:07 +0900

    validates datetime

--- crimemap.py
+++ crimemap.py
@@ -1,6 +1,8 @@
 from dbhelper import DBHelper
 from flask import Flask, render_template, request
 import json
+import dateparser
+import datetime
 
 
 app = Flask(__name__)
@@ -12,10 +14,10 @@ categories = [
 
 
 @app.route('/')
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes, categories=categories)
+    return render_template("home.html", crimes=crimes, categories=categories, error_message=error_message)
 
 
 @app.route('/add', methods=["POST"])
@@ -42,7 +44,9 @@ def submitcrime():
     category = request.form.get("category")
     if category not in categories:
         return home()
-    date = request.form.get("date")
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format")
     try:
         latitude = float(request.form.get("latitude"))
         longitude = float(request.form.get("longitude"))
@@ -51,6 +55,15 @@ def submitcrime():
     description = request.form.get("description")
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
+
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
