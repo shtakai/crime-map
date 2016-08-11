20b2e21696db1051c2a546147ab066d85f2ee657
shtakai <shtakai@gmail.com>
2016-08-11 16:49:01 +0900

    add method 'get_all_crimes'

--- mockdbhelper.py
+++ mockdbhelper.py
@@ -34,3 +34,12 @@ class DBHelper:
 
     def add_crime(self, category,  date, latitude, longitude, description):
         pass
+
+    def get_all_crimes(self):
+        return[{
+            'latitude': -33.301304,
+            'longitude': 26.523355,
+            'date': '2000-01-01',
+            'category': 'mugging',
+            'description', 'mock description'
+        }]
